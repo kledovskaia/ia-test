e281e8153d2231a90eb97bfe0468ec08dbb16628
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-30 11:06:22 +0300

    added: scroll buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { DetailedHTMLProps, FC, HTMLAttributes, useState } from 'react';
+import type { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
 import { useEffect, useRef } from 'react';
 import { useMessages } from './hooks/useMessages';
 import cn from 'classnames';
 import styles from './App.module.scss';
 import Message from './components/Message/Message';
 import MessagesFeed from './components/MessagesFeed/MessagesFeed';
+import { ReactComponent as ArrowIcon } from './assets/arrow-down.svg';
+
+const handleScrollTop = () => {
+  window.scroll({ left: 0, top: 0, behavior: 'smooth' });
+};
+const handleScrollBottom = () => {
+  window.scroll({
+    left: 0,
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  });
+};
 
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
@@ -12,21 +24,24 @@ const App: FC<Props> = ({ className, ...props }) => {
   const { data: messages, loading, error } = useMessages();
   const lastMessage = useRef<HTMLLIElement>(null);
   const previousOffsetTop = useRef(0);
+  const airSpace = useRef<HTMLButtonElement>(null);
   const messagesContainer = useRef<HTMLDivElement>(null);
-  const airSpace = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!lastMessage.current) return;
+    if (!messagesContainer.current) return;
     if (!airSpace.current) return;
     const difference = Math.abs(
-      previousOffsetTop.current - airSpace.current.offsetTop
+      previousOffsetTop.current -
+        (lastMessage.current.offsetTop + lastMessage.current.offsetHeight)
     );
 
     const shouldScroll =
       document.body.offsetHeight - window.scrollY - window.innerHeight <=
       difference;
 
-    previousOffsetTop.current = airSpace.current.offsetTop;
+    previousOffsetTop.current =
+      lastMessage.current.offsetTop + lastMessage.current.offsetHeight;
 
     if (shouldScroll) {
       lastMessage.current.scrollIntoView({ behavior: 'smooth' });
@@ -34,21 +49,39 @@ const App: FC<Props> = ({ className, ...props }) => {
   }, [messages]);
 
   return (
-    <div className={cn(className, styles.container)} {...props}>
-      <div ref={messagesContainer}>
-        <MessagesFeed>
-          {messages.map((message, index) => (
-            <li
-              key={`${index}-${message.id}`}
-              ref={index === messages.length - 1 ? lastMessage : null}
-            >
-              <Message message={message} />
-            </li>
-          ))}
-        </MessagesFeed>
-        <div ref={airSpace}></div>
+    <>
+      <div className={cn(className, styles.app)} {...props}>
+        <button
+          className={cn(styles.app__button, styles.app__button_down)}
+          onClick={handleScrollBottom}
+        >
+          <span>
+            <ArrowIcon />
+          </span>
+        </button>
+        <div className={styles.app__content} ref={messagesContainer}>
+          <MessagesFeed>
+            {messages.map((message, index) => (
+              <li
+                key={`${index}-${message.id}`}
+                ref={index === messages.length - 1 ? lastMessage : null}
+              >
+                <Message message={message} />
+              </li>
+            ))}
+          </MessagesFeed>
+        </div>
+        <button
+          className={cn(styles.app__button, styles.app__button_up)}
+          ref={airSpace}
+          onClick={handleScrollTop}
+        >
+          <span>
+            <ArrowIcon />
+          </span>
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
